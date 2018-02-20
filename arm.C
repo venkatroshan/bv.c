6b97f6cc54fd85e331e67193dca4702ac121bdb8
venkatroshan <35727735+venkatroshan@users.noreply.github.com>
2018-02-20 03:01:31 -0800

    Update arm.C

--- arm.C
+++ arm.C
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<conio.h>
-int main()
+void main()
 {
 int n,m,i,j,rem;
 scanf("%d %d",&n,&m);
-getch();
+
 for(i=n;i<m;i++)
 {
 int p=i,sum=0;
@@ -19,5 +19,5 @@ for(j=0;p!=0;j++)
      printf("%d\n",sum);
 
 }
-return 0;
+getch();
 }
